8d6b79c92c020e3ab6632a6358b11f36769ad90c
charles-42 <71760090+charles-42@users.noreply.github.com>
2022-06-20 16:35:29 +0200

    change proxy

--- channel_app/__init__.py
+++ channel_app/__init__.py
@@ -2,8 +2,10 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 app.config.from_object('config')
 
 # Create database connection object
